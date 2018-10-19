326aa66d54a57837a45944df094cf256d390844f
Simone Zugarini <zuga_simo@hotmail.it>
2018-10-19 20:33:44 +0200

    Modifica 2

---
+++ Compiti/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valore1 = 65555;
+            short numero = (short)valore1;
+            Console.WriteLine(numero);
+            
+
+
+
+
+            Task t = new Task(Write);
+            t.Start();
+
+            int i = 1000;
+            while (i > 0)
+            {
+                Console.Write("Settembre");
+                i--;
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        public static void Write()
+        {
+            int i = 1000;
+            while (i > 0)
+            {
+                Console.Write(i);
+                i--;
+            }
+        }
+        
+
+    }
+}
